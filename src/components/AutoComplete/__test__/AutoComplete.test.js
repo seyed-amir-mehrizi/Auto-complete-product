26bfe1b822bd9b222ec69ecc3ccd0b0e9a1f2072
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2021-12-24 19:51:02 +0330

    write test for the auto-complete component

---
+++ src/components/AutoComplete/__test__/AutoComplete.test.js
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import AutoComplete from '../AutoComplete'
+
+describe('AutoComplete Component', () => {
+    it('should have input by the place holder text', async () => {
+        render(<AutoComplete />);
+        const autoCompleteElement = screen.getByPlaceholderText(/Search The Product/i);
+        expect(autoCompleteElement).toBeInTheDocument();
+    });
+
+    it('should change the value in the input when user type', async () => {
+        render(<AutoComplete />);
+        const autoCompleteElement = screen.getByPlaceholderText(/Search The Product/i);
+        fireEvent.change(autoCompleteElement, { target: { value: 'week' } });
+        expect(autoCompleteElement.value).toBe('week')
+    })
+})
+
+
