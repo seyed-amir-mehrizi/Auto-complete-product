d8dcb7081266c945e21e840bb2fab18bdd353e02
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2021-12-24 16:42:54 +0330

    fetch data from remote server

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 import AutoComplete from './components/AutoComplete/AutoComplete';
 import Table from './components/Table/Table';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function App() {
+const [allData , setAllData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  },[])
+
+
+  const fetchData = async () => {
+    const result = await (await axios.get('http://localhost:3001/products')).data;
+    setAllData(result);
+    
+  }
   return (
     <div className="App h-100">
       <div className='d-flex h-100'>
@@ -15,7 +28,7 @@ function App() {
           <div className='sticky-top'>
             <AutoComplete />
           </div>
-          <div className='flex-grow-1 p-2' style={{msOverflowY:'auto'}}>
+          <div className='flex-grow-1 p-2' style={{ msOverflowY: 'auto' }}>
             <Table />
           </div>
         </div>
