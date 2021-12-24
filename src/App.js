8c4bfd233cb3eaca995d8b7f64ce5b82c6e27388
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2021-12-24 17:16:49 +0330

    make functionality for the auto complete search

--- src/App.js
+++ src/App.js
@@ -6,23 +6,41 @@ import Table from './components/Table/Table';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 function App() {
-const [allData , setAllData] = useState([]);
-const [searchTitle , setSearchTitle] = useState('');
+  const [allData, setAllData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
+  const [searchTitle, setSearchTitle] = useState('');
 
   useEffect(() => {
     fetchData();
-  },[])
+  }, [])
+
+  useEffect(() => {
+    setFilterData(makeFilterProducts(allData))
+  }, [searchTitle]);
 
 
   const fetchData = async () => {
     const result = await (await axios.get('http://localhost:3001/products')).data;
     setAllData(result);
-    
+
   }
 
-  const handleSearchTitle = (e) =>{
+  const handleSearchTitle = (e) => {
     setSearchTitle(e);
-    console.log(e);
+    if (e.length > 3) {
+      setTimeout(() => {
+        const FilterByTitle = makeFilterProducts(allData);
+        setFilterData(FilterByTitle);
+      }, 300);
+    }
+    // console.log(e);
+  }
+
+
+  const makeFilterProducts = (array) => {
+    return array.filter((item) => {
+      return item.title.toLowerCase().includes(searchTitle.toLowerCase());
+    })
   }
   return (
     <div className="App h-100">
@@ -32,10 +50,10 @@ const [searchTitle , setSearchTitle] = useState('');
         </div>
         <div className='flex-grow-1 flex-column w-100'>
           <div className='sticky-top'>
-            <AutoComplete getSearchTitle={handleSearchTitle}/>
+            <AutoComplete getSearchTitle={handleSearchTitle} />
           </div>
           <div className='flex-grow-1 p-2' style={{ msOverflowY: 'auto' }}>
-            <Table/>
+            <Table Products={filterData} />
           </div>
         </div>
       </div>
