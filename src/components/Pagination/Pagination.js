530dd41dd30abe4da1ef6f54d591e52d7262a387
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2021-12-26 11:16:15 +0330

    write unit test for the pagination component

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,20 +1,21 @@
 import React from 'react'
 
-export default function Pagination({productsPerPage , totalProducts , paginate , currentPage}) {
+export default function Pagination({ productsPerPage, totalProducts, paginate, currentPage }) {
     const pageNumber = [];
     for (let index = 1; index <= Math.ceil(totalProducts / productsPerPage); index++) {
         pageNumber.push(index);
-        
+
     }
-    
+
     return (
         <nav className='d-flex justify-content-center' data-testid="pagination-navbar">
             <ul className='pagination flex-wrap'>
                 {
-                    pageNumber.map((number)=>{
+                    pageNumber.map((number, i) => {
                         const activeClass = currentPage === number ? 'active' : '';
-                        return <li key={number} className={`m-1 page-item ${activeClass}`} onClick={()=>paginate(number)}>
-                            <a href='!#' className="page-link">
+                        return <li key={number} className={`m-1 page-item ${activeClass}`}
+                            onClick={() => paginate(number)}>
+                            <a href='!#' className="page-link" data-testid={`anchor-item`}>
                                 {number}
                             </a>
                         </li>
--- src/components/Pagination/__test__/Pagination.test.js
+++ src/components/Pagination/__test__/Pagination.test.js
@@ -1,11 +1,40 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Pagination from '../Pagination'
 
+function PaginationList() {
+    const numbers = [1, 2, 3];
+    return (
+        numbers.map((item, i) => {
+            return <><li key={i}>
+                <a href='#!' data-testid={`item-${i}`}>
+                    {item}
+                </a>
+            </li></>
+        })
+
+    )
+}
 describe('AutoComplete Component', () => {
+    beforeAll(() => {
+        const paginate = jest.fn()
+        render(<Pagination paginate={paginate} />);
+    })
     it('should have input by the place holder text', async () => {
-        render(<Pagination />);
         const navElement = screen.getByTestId(/pagination-navbar/i);
         expect(navElement).toBeInTheDocument();
+    });
+
+    it('should have list of pagination in the document', async () => {
+        render(<PaginationList />);
+        const anchorElements = await screen.findByTestId('item-0');
+        expect(anchorElements).toBeInTheDocument();
+    });
+
+    it('should change the page when click on item', async () => {
+        render(<PaginationList />);
+        const anchorElements = await screen.findByTestId('item-0');
+        fireEvent.click(anchorElements)
+        expect(anchorElements).not.toBeNull();
     });
 
 
