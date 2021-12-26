31a463e89497f07792c3d5af87e523bbffacca8d
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2021-12-26 14:52:02 +0330

    write integration test for the app component

--- src/App.test.js
+++ src/App.test.js
@@ -6,8 +6,34 @@ import Table from './components/Table/Table'
 import { render, screen, fireEvent } from '@testing-library/react';
 
 describe('CustomComponent', () => {
-    it('should be in the document',() => {
+    it('table should render in the document when on sale checkbox is changed',() => {
+        render(<App />);
+        const checkboxElement = screen.getByTestId('on-sale-checkbox');
+        checkboxElement.click(checkboxElement);
+        const filterData = [1,2,3];
+        render(<Table Products={filterData}/>);
+        const tableElement = screen.getByTestId('products-table');
+        expect(tableElement).toBeInTheDocument();
+    });
 
+    it('table should render in the document when dropdown item change',() => {
+        render(<App />);
+        const dropdownelement = screen.getByTestId('gender-dropdown');
+        fireEvent.change(dropdownelement, { target: { name: 'female' } });
+        const filterData = [1,2,3];
+        render(<Table Products={filterData}/>);
+        const tableElement = screen.getByTestId('products-table');
+        expect(tableElement).toBeInTheDocument();
+    });
+
+    it('table should render in the document when user write on the auto complete',() => {
+        render(<App />);
+        const autoCompleteElement = screen.getByPlaceholderText(/Search The Product/i);
+        fireEvent.change(autoCompleteElement, { target: { value: 'week' } });
+        const filterData = [1,2,3];
+        render(<Table Products={filterData}/>);
+        const tableElement = screen.getByTestId('products-table');
+        expect(tableElement).toBeInTheDocument();
     });
 
 
