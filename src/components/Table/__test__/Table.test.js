8e6ce4decaae33ea6c050a5fdb52bc1a8af8f9d7
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2021-12-26 14:36:38 +0330

    unit test for table component

--- src/components/Table/__test__/Table.test.js
+++ src/components/Table/__test__/Table.test.js
@@ -8,4 +8,11 @@ describe('Table Component', () => {
         const headingElement = screen.getByRole('heading' , {name : /There Are No Products/i});
         expect(headingElement).toBeInTheDocument();
     })
+    it('should render the the table element', async() => {
+        const data = [1,2,3]
+        render(<Table Products={data}/>);
+        const tableElement = await screen.findByTestId('products-table');
+        expect(tableElement).toBeInTheDocument();
+    })
+
 })  
