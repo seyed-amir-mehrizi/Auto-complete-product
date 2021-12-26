5dda6b08e6ce1802c7849bb21e463bf14d3504f1
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2021-12-26 12:00:07 +0330

    had to revet the project because of some issues

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
@@ -13,3 +14,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
